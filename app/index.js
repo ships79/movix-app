3fa5338dc37bec5ab749416c0799bed75df0f173
Abdessamad <abdessamadchabadi@gmail.com>
2025-12-07 11:50:10 +0100

    Initial commit for Movix Player

---
+++ app/index.js
@@ -0,0 +1,24 @@
+import { StyleSheet, View } from "react-native";
+import { WebView } from "react-native-webview";
+
+export default function Index() {
+  return (
+    <View style={styles.container}>
+      <WebView
+        source={{ uri: "https://horizons.hostinger.com/9a486861-0d87-4ec8-8637-f8016528cc72" }}
+        allowsInlineMediaPlayback={true}
+        mediaPlaybackRequiresUserAction={false}
+        javascriptEnabled={true}
+        domStorageEnabled={true}
+        allowsFullscreenVideo={true}
+        style={{ flex: 1 }}
+      />
+    </View>
+  );
+}
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+  },
+});
